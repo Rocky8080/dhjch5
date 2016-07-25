95c15af11a806b0b401161a9b27a1724b7fbf9d0
Rocky Xia <xiali80@gmail.com>
2016-07-26 00:28:43 +0800

    fix

--- server/routes/locto.js
+++ server/routes/locto.js
@@ -20,66 +20,62 @@ var signature;
 
 function refresh_token(){
     client.on('connect', function() {
+    });
 
+    client.get('token', function(err, reply) {
+        token = reply;
+        console.log('get token : ' + token);
 
-        client.get('token', function(err, reply) {
-            token = reply;
-            console.log('get token : ' + token);
+        if (!token){
+            http.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appId + "&secret=" + APPSECRET, function(response) {
+                // Continuously update stream with data
+                var body = '';
+                response.on('data', function(d) {
+                    body += d;
+                });
+                response.on('end', function() {
 
-            if (!token){
-                http.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appId + "&secret=" + APPSECRET, function(response) {
+                    // Data reception is done, do whatever with it!
+                    var parsed = JSON.parse(body);
+                    token = parsed.access_token
+                    client.set('token', token, redis.print);
+                    client.expire('token', 7200);
+                    console.log('set token : ' + token);
+                });
+            }).on('error', function(e) {
+                console.log("Got error: " + e.message);
+            });
+        }
+
+        // 初始化ticket
+        client.get('ticket', function(err, reply) {
+            ticket = reply;
+            console.log('get ticket : ' + ticket);
+
+            if (ticket == 'undefined' || ticket == undefined || ticket == null){
+                http.get("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + token +"&type=jsapi", function(response) {
                     // Continuously update stream with data
                     var body = '';
                     response.on('data', function(d) {
                         body += d;
                     });
                     response.on('end', function() {
-
+                        console.log('ticket body : ' + body);
                         // Data reception is done, do whatever with it!
                         var parsed = JSON.parse(body);
-                        token = parsed.access_token
-                        client.set('token', token, redis.print);
-                        client.expire('token', 7200);
-                        console.log('set token : ' + token);
+                        ticket = parsed.ticket
+                        if (ticket != 'undefined' && ticket != undefined && ticket != null) {
+                            client.set('ticket', ticket, redis.print);
+                            client.expire('ticket', 7200);
+                        }
+                        console.log('set ticket' + ticket);
                     });
                 }).on('error', function(e) {
                     console.log("Got error: " + e.message);
                 });
             }
-
-            // 初始化ticket
-            client.get('ticket', function(err, reply) {
-                ticket = reply;
-                console.log('get ticket : ' + ticket);
-
-                if (ticket == 'undefined' || ticket == undefined || ticket == null){
-                    http.get("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + token +"&type=jsapi", function(response) {
-                        // Continuously update stream with data
-                        var body = '';
-                        response.on('data', function(d) {
-                            body += d;
-                        });
-                        response.on('end', function() {
-                            console.log('ticket body : ' + body);
-                            // Data reception is done, do whatever with it!
-                            var parsed = JSON.parse(body);
-                            ticket = parsed.ticket
-                            if (ticket != 'undefined' && ticket != undefined && ticket != null) {
-                                client.set('ticket', ticket, redis.print);
-                                client.expire('ticket', 7200);
-                            }
-                            console.log('set ticket' + ticket);
-                        });
-                    }).on('error', function(e) {
-                        console.log("Got error: " + e.message);
-                    });
-                }
-            });
         });
-
     });
-
-
 }
 
 /* GET home page. */
@@ -87,9 +83,9 @@ router.get('/locto.html', function(req, res) {
     console.log('original url : ' + (req.originalUrl));
     console.log('encode url : ' + encodeURI(req.originalUrl));
     console.log('decode url : ' + decodeURI(req.originalUrl));
-    refresh_token();
     var fullUrl = req.protocol + '://' + req.get('host') + (req.originalUrl);
     console.log('full_url:' + fullUrl);
+    refresh_token();
 
     var sign_params = sign(ticket, fullUrl);
     console.log(sign_params);
